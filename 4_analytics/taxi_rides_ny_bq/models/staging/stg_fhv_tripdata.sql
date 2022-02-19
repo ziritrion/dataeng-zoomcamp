47045979d2d1057345e0306e5a7ca88e53f6a40c
Alvaro Navas <ziritrion@gmail.com>
2022-02-19 20:40:26 +0100

    Lesson 4 homework code

---
+++ 4_analytics/taxi_rides_ny_bq/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,30 @@
+{{ config(materialized='view') }}
+
+-- with tripdata as 
+-- (
+--   select *,
+--     row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+--   from {{ source('staging','fhv_tripdata_clustered') }}
+--   where dispatching_base_num is not null 
+-- )
+select
+    -- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    cast(dispatching_base_num as integer) as vendorid,
+    cast(pulocationid as integer) as  pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+
+    -- trip info
+    sr_flag,
+from {{ source('staging','fhv_tripdata_clustered') }}
+where dispatching_base_num is not null 
+
+{% if var('is_test_run', default=true) %}
+
+    limit 100
+
+{% endif %}
