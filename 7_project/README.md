327622190cba70e4014f1906de542b269bad3137
Alvaro Navas <ziritrion@gmail.com>
2022-03-26 13:17:54 +0100

    Final project, terraform files first draft

---
+++ 7_project/README.md
@@ -0,0 +1,8 @@
+# Data Engineering Zoomcamp Project
+
+This folder contains my project for the [Data Engineering Zoomcamp](https://github.com/DataTalksClub/data-engineering-zoomcamp) by [DataTalks.Club](https://datatalks.club).
+
+# Dataset
+
+The chosen dataset for this project is the [GitHub Archive](https://www.gharchive.org/).
+
---
+++ 7_project/terraform/variables.tf
@@ -0,0 +1,35 @@
+locals {
+  data_lake_bucket = "data_lake"
+}
+
+variable "project" {
+  description = "Your GCP Project ID"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "europe-west1"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "gh_archive_all"
+}
+
+# Transfer service
+# variable "access_key_id" {
+#   description = "AWS access key"
+#   type = string
+# }
+
+# variable "aws_secret_key" {
+#   description = "AWS secret key"
+#   type = string
+# }
